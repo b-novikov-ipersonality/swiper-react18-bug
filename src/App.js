03179963fd13b9fa4344e22811be315c9389dd0d
Boris Novikov <boris.novikov95@gmail.com>
2022-04-18 12:26:16 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [swiper, setSwiper] = useState();
+  useEffect(() => {
+    if (swiper) {
+      swiper.on('slideChange', (s) => {
+        console.log(s);
+      })
+    }
+  }, [swiper])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Swiper onSwiper={setSwiper}>
+        <SwiperSlide>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </SwiperSlide>
+        <SwiperSlide>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Or don't learn React
+            </a>
+          </header>
+        </SwiperSlide>
+        <SwiperSlide>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              I don't care
+            </a>
+          </header>
+        </SwiperSlide>
+        <SwiperSlide>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Do whatever you want
+            </a>
+          </header>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
